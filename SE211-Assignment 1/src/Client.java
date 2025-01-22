1a84b34fb0e1f06aa1720889ab563192bd297e4c
Chris <32879583+godofthecrabz@users.noreply.github.com>
2025-01-22 10:14:49 -0500

    1/22/2025
    Wrote some code

--- SE211-Assignment 1/src/Client.java
+++ SE211-Assignment 1/src/Client.java
@@ -1,2 +1,38 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
 public class Client {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Port must be specified");
+            System.exit(-1);
+        }
+        int port = Integer.parseInt(args[0]);
+        PrintWriter out = null;
+        Socket socket = null;
+        try {
+            socket = new Socket("localhost", port);
+            out = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            System.out.println("Could not connect to server");
+            System.exit(-1);
+        }
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter command: ");
+        String userInput = in.nextLine();
+        while (!userInput.equals("quit")) {
+            out.println(userInput);
+            System.out.print("Enter command: ");
+            userInput = in.nextLine();
+        }
+        out.println("quit");
+        in.close();
+        out.close();
+        try {
+            socket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- SE211-Assignment 1/src/Server.java
+++ SE211-Assignment 1/src/Server.java
@@ -1,2 +1,41 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
 public class Server {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Port must be specified");
+            System.exit(-1);
+        }
+        int port = Integer.parseInt(args[0]);
+        File logFile = new File("log.txt");
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        Scanner scanner = null;
+        try {
+            serverSocket = new ServerSocket(port);
+            clientSocket = serverSocket.accept();
+            scanner = new Scanner(clientSocket.getInputStream());
+            logFile.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Could not listen on port: " + port);
+            System.exit(-1);
+        }
+        String inputLine;
+        while ((inputLine = scanner.nextLine()) != null) {
+            System.out.print("The Client Printed: ");
+            System.out.println(inputLine);
+            if (inputLine.equals("quit")) {
+                break;
+            }
+        }
+        scanner.close();
+        try {
+            clientSocket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
